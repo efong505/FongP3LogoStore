1498b6c55186e10370ada8d78a6de3ff99f5781a
EDWARD FONG <efong@cnm.edu>
2020-06-09 16:48:21 -0600

    Add LogoOrderItem Class

---
+++ FongP3LogoStore/LogoOrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FongP3LogoStore
+{
+    class LogoOrderItem
+    {
+    }
+}
